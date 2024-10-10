06cb38f7645597b85dec9c1a519e3a0649f7a90f
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-10-10 17:05:43 +0300

    Update Task2_1.js
    Added case 2

--- Task2/Task2_1.js
+++ Task2/Task2_1.js
@@ -1,4 +1,5 @@
 // Prepare promise based alternative
+// Write use cases for the promise based solution
 
 function promiseMap(array, callback) {
     const promises = array.map(item => callback(item));
@@ -6,6 +7,9 @@ function promiseMap(array, callback) {
 }
 
 async function demoFunc() {
+
+    
+    // case 1
     const numbers = [1, 2, 3, 4, 5];
 
     const promiseDouble = (num) => {
@@ -19,6 +23,20 @@ async function demoFunc() {
             console.log(results);
         });
 
+    
+    //case 2
+    const numbers2 = [10, 20, 30];
+
+    const promiseSquare = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num * num), Math.random() * 1000);
+        });
+    };
+    
+    promiseMap(numbers2, promiseSquare)
+    .then(squares => {
+        console.log(squares);
+    });
 }
 
 demoFunc();
