20b16ca841f5538a3b62e4a2a0e6dfb3c044a4c7
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-10-10 16:59:22 +0300

    Update Task2_1.js
    Prepared promise based alternative

--- Task2/Task2_1.js
+++ Task2/Task2_1.js
@@ -1,1 +1,24 @@
+// Prepare promise based alternative
 
+function promiseMap(array, callback) {
+    const promises = array.map(item => callback(item));
+    return Promise.all(promises);
+}
+
+async function demoFunc() {
+    const numbers = [1, 2, 3, 4, 5];
+
+    const promiseDouble = (num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(num * 3), Math.random() * 1000);
+        });
+    };
+
+    promiseMap(numbers, promiseDouble)
+        .then(results => {
+            console.log(results);
+        });
+
+}
+
+demoFunc();
