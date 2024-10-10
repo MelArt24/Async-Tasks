a52e30e47dd46c5e57857ccddac01d2d2ab04672
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-10-10 18:00:00 +0300

    Update Task2_1.kt
    1) Prepared promise based alternative
    2) Wrote use case for the promise based solution

--- Task2/Task2_1.kt
+++ Task2/Task2_1.kt
@@ -1,1 +1,25 @@
+// Prepared promise based alternative
+// Wrote use case for the promise based solution
 
+import kotlinx.coroutines.*
+
+fun <T, R> promiseMap(scope: CoroutineScope, list: List<T>, callback: (T) -> Deferred<R>): Deferred<List<R>> {
+    return scope.async {
+        list.map { callback(it).await() }
+    }
+}
+
+fun main() = runBlocking {
+    val numbers = listOf(1, 2, 3, 4, 5)
+
+    val promiseTriple: (Int) -> Deferred<Int> = { num ->
+        CompletableDeferred<Int>().apply {
+            launch {
+                delay(1000)
+                complete(num * 3)
+            }
+        }
+    }
+    
+    val results = promiseMap(this, numbers, promiseTriple).await()
+    println(results)
