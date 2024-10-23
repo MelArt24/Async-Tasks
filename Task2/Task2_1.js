272dd16b187db82b886e899bee28eaf4102d63f3
MelArt24 <143939388+MelArt24@users.noreply.github.com>
2024-10-23 22:20:35 +0300

    Update Task2_1.js

--- Task2/Task2_1.js
+++ Task2/Task2_1.js
@@ -12,7 +12,7 @@ async function demoFunc() {
     const numbers = [1, 2, 3, 4, 5];
 
     const promiseTriple = (num) => {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (num === 3) {
                     reject(`Error: Cannot process number ${num}`);
@@ -26,7 +26,7 @@ async function demoFunc() {
     promiseMap(numbers, promiseTriple)
         .then(results => {
             console.log(results);
-        });
+        })
         .catch(error => {
             console.error('Error in case 1:', error);
         });
@@ -36,7 +36,7 @@ async function demoFunc() {
     const numbers2 = [10, 20, 30];
 
     const promiseSquare = (num) => {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (num === 20) {
                     reject(`Error: Cannot process number ${num}`);
@@ -50,7 +50,7 @@ async function demoFunc() {
     promiseMap(numbers2, promiseSquare)
         .then(squares => {
             console.log(squares);
-        });
+        })
         .catch(error => {
             console.error('Error in case 2:', error);
         });
